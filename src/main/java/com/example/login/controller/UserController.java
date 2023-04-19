9409f7c0dbbe18743e2d5d83466a9132c118279b
wangzhifeng <41932817+ohuo133233@users.noreply.github.com>
2023-04-19 18:45:52 +0800

    Update UserController.java

--- src/main/java/com/example/login/controller/UserController.java
+++ src/main/java/com/example/login/controller/UserController.java
@@ -46,4 +46,64 @@ public class UserController {
         return userService.login(user);
     }
 
+
+    @GetMapping(value = "/login_dev")
+    public String login_dev(@RequestParam String userName, @RequestParam String password, @RequestParam String userType, @RequestParam String rememberMe) {
+
+        logger.info("调用login_test接口,传入的参数: userName" + userName + "password: " + password + "userType： " + userType + "rememberMe: " + rememberMe);
+        String m = "  {\n" +
+                "        \"stateCode\":200,\n" +
+                "            \"message\":\"请求成功\",\n" +
+                "            \"data\":{\n" +
+                "        \"userId\":\"201223232\",\n" +
+                "                \"userName\":\"Alan\",\n" +
+                "                \"realName\":\"Peter\",\n" +
+                "                \"sex\":\"0\",\n" +
+                "                \"birthday\":\"2013-12-24\",\n" +
+                "        “avatar”:\"https://wwddadasadsad.sa\",\n" +
+                "                \"enrollmentDate\":\"2020-08-27\",\n" +
+                "                \"schoolId\":\"200012120\",\n" +
+                "                \"schoolName\":\"完全小学\",\n" +
+                "                \"gradeId\":\"200012120\",\n" +
+                "                \"gradeName\":\"三年级\",\n" +
+                "                \"classId\":\"200012120\",\n" +
+                "                \"className\":\"一班\",\n" +
+                "                \"score\":\"100\"\n" +
+                "        \"remark\":\"备注\"\n" +
+                "    }\n" +
+                "    }";
+        return m;
+
+    }
+
+    @GetMapping(value = "/login_test")
+    public String login_test() {
+
+//        logger.info("调用login_test接口,传入的参数: userName" + userName + "password: " + password + "userType： " + userType + "rememberMe: " + rememberMe);
+        String m = "  {\n" +
+                "        \"stateCode\":200,\n" +
+                "            \"message\":\"请求成功\",\n" +
+                "            \"data\":{\n" +
+                "        \"userId\":\"201223232\",\n" +
+                "                \"userName\":\"Alan\",\n" +
+                "                \"realName\":\"Peter\",\n" +
+                "                \"sex\":\"0\",\n" +
+                "                \"birthday\":\"2013-12-24\",\n" +
+                "        “avatar”:\"https://wwddadasadsad.sa\",\n" +
+                "                \"enrollmentDate\":\"2020-08-27\",\n" +
+                "                \"schoolId\":\"200012120\",\n" +
+                "                \"schoolName\":\"完全小学\",\n" +
+                "                \"gradeId\":\"200012120\",\n" +
+                "                \"gradeName\":\"三年级\",\n" +
+                "                \"classId\":\"200012120\",\n" +
+                "                \"className\":\"一班\",\n" +
+                "                \"score\":\"100\"\n" +
+                "        \"remark\":\"备注\"\n" +
+                "    }\n" +
+                "    }";
+        return m;
+
+    }
+
+
 }
